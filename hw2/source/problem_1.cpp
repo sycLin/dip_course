47e3983381fdf51f4e14b86053bca74fe7e14b8e
sycLin <b01902044@ntu.edu.tw>
2016-04-11 01:32:14 +0800

    for problem 1

---
+++ hw2/source/problem_1.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <string>
+#include "ImageUtil.h"
+#include "ImageUtil.cpp"
+
+void print_usage();
+
+int main(int argc, char* argv[]) {
+
+	// check arguments
+	if(argc != 4) {
+		cerr << "wrong arguments!" << endl;
+		print_usage();
+		exit(-1);
+	}
+
+	unsigned char** sample1 = (unsigned char**)malloc(256 * sizeof(unsigned char*));
+	for(int i=0; i<256; i++)
+		sample1[i] = (unsigned char*)malloc(256 * sizeof(unsigned char));
+
+	if(!openImageAsMatrix(argv[1], sample1, 256, 256)) {
+		cerr << "cannot open image at: " << argv[1] << endl;
+		exit(-1);
+	}
+
+	// do Canny edge detection on sample1
+	unsigned char** sample1_canny = (unsigned char**)malloc(256 * sizeof(unsigned char*));
+	for(int i=0; i<256; i++)
+		sample1_canny[i] = (unsigned char*)malloc(256 * sizeof(unsigned char));
+	if(!doCannyEdgeDetector(sample1, sample1_canny, 256, 256, 250.0, 25.0)) {
+		cerr << "cannot perform canny edge detection on sample1..." << endl;
+		exit(-1);
+	}
+
+	// write results to file
+	if(!writeImageToFile("sample1_canny.raw", sample1_canny, 256, 256)) {
+		cerr << "cannot write image to file (sample1_canny.raw)..." << endl;
+		exit(-1);
+	}
+
+
+	return 0;
+}
+
+void print_usage() {
+	cerr << "Usage:" << endl;
+	cerr << "\t./a.out <sample1> <sample2> <sample3>" << endl;
+}
+
+
